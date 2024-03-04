bd14d62271aacaa7c827bc3e95e8627dcbea306e
Mikh Villanueva <mikhael.villanueva@student.ateneo.edu>
2024-03-04 23:20:17 +0800

    updated to lab2 specs

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, RecipeIngredient
 
 # Register your models here.
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    inlines = [RecipeIngredientInline, ]
+
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
